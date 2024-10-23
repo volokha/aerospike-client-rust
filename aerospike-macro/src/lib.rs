87ee16af14be0a032b8b9bc90f5beb02b5512c24
Galphimbl <sashatkachov91@gmail.com>
2024-10-23 16:40:56 +0300

    small fixes

--- aerospike-macro/src/lib.rs
+++ aerospike-macro/src/lib.rs
@@ -1,7 +1,6 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::parse_macro_input;
 #[doc(hidden)]
 #[proc_macro_attribute]
 pub fn test(_attr: TokenStream, input: TokenStream) -> TokenStream {
